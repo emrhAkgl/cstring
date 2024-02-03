2c4077c717b270b91621d42b72a9dd5a51f5fc03
Emrah Akgül <45803976+emrhAkgl@users.noreply.github.com>
2024-02-03 18:55:11 +0300

    Update test.c

--- test.c
+++ test.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include "mystring.h"
 
+void test_init_string()
+{
+        string *test_string = init_string();
+        if(!test_string) {
+                fprintf(stderr, "test_init_string: hafıza tahsis edilemedi.\n");
+        } else {
+                free(test_string);
+        }
+}
