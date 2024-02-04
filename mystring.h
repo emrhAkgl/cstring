6f56a7b1f1c248328bf7659e9b5eac3ab7b9adfa
Emrah Akgül <45803976+emrhAkgl@users.noreply.github.com>
2024-02-04 10:35:58 +0300

    Update mystring.h

--- mystring.h
+++ mystring.h
@@ -3,6 +3,7 @@
 
 #include <sys/types.h>
 
+//This struct is designed to use the string object from other languages in C.
 typedef struct string_s {
 	char 			*data;
 	size_t 			size;
@@ -13,10 +14,25 @@ typedef struct string_s {
 	void			(*free_string)(struct string_s *str);
 } string;
 
+// To apply the corresponding functions to function pointers in the string_s structure.
 string *init_string();
 
+// To add a string to the data pointer in the string_s structure. 
+// If the pointer is empty it allocates memory, if it is not empty 
+// it adds a space at the end of the last word in it and adds the data
+// in "_data" to string->data.
 size_t add_string(string *str, const char *_data);
+
+// It does the same job as the add_string function. The only difference is that 
+// instead of giving a value with the _data parameter, we get it from the terminal
+// with the user's input. Since it does not request the user to enter a value 
+// (each user may want to give a different message, so the function can be edited 
+// in the future and this situation can be solved), each user must give his own message 
+// before the function call. 
 size_t add_string_from_terminal(string *str);
+
+// Finally, we release the allocated memory again with functions like "init_string, 
+// add_string, add_string_from_terminal". 
 void free_string(string *str);
 
 
