861e8a7f9e522011f20df3ca7d3232492760844e
Emrah Akgül <45803976+emrhAkgl@users.noreply.github.com>
2024-02-05 18:07:26 +0300

    Update mystring.c

--- mystring.c
+++ mystring.c
@@ -20,72 +20,114 @@ string *init_string()
 
 size_t add_string(string *str, const char *_data)
 {
-	if(_data == NULL)
+	char *str_data_buf = NULL;
+
+	// _data boşsa veya MAX_STRING_SIZE'dan büyükse 0 döndür.
+	if(_data == NULL || (strlen(_data) + str->size) > MAX_STRING_SIZE) 	
 		return 0;
-	if(str->size == 0) {
-		str->data = strdup(_data);
-		if(str->data == NULL) {
+	
+	// str'ın data üyesi boşsa hafıza tahsisi ile _data'yı üyeye ekle.
+	if(str->size == 0) {	
+		if ((str->data = strdup(_data)) == NULL) {
 			return 0;
 		} else {
 			str->size = strlen(str->data)+1;
 			return str->size;
 		}
+	}
+	
+	//'str->data' doluysa sonuna bir boşlukla beraber '_data' yı ekle.
+	if((str_data_buf = strdup(str->data)) == NULL) {
+		return 0;
 	} else {
-		char *str_data_buf = strdup(str->data);
-		if(str_data_buf == NULL) {
+		free(str->data);
+		str->data = NULL;
+		
+		str->data = (char *)calloc(((strlen(str_data_buf) + strlen(_data)) + 1), sizeof(char));
+		if(str->data == NULL) {
+			free(str_data_buf);
 			return 0;
 		} else {
-			free(str->data);
-			str->data = NULL;
-			str->data = (char *)calloc(((strlen(str_data_buf) + strlen(_data)) + 1), sizeof(char));
-			
-			if(str->data == NULL) {
-				free(str_data_buf);
-				return 0;
-			} else {
-				sprintf(str->data, "%s %s", str_data_buf, _data);
-				str->size = strlen(str->data) + 1;
-				return (size_t)(strlen(str->data) + 1);
-			}
-		}	
-	}
+			sprintf(str->data, "%s %s", str_data_buf, _data);
+			str->size = strlen(str->data) + 1;
+			free(str_data_buf);
+			str_data_buf = NULL;
+			return (size_t)(strlen(str->data) + 1);
+		}
+	}		
 }
 
-size_t add_string_from_terminal(string *str) {
-	char buf[2048];
-
-	if (fgets(buf, sizeof(buf), stdin) == NULL) {
+size_t add_string_from_terminal(string *str) {		// BURASI İLE İLGİLEN!
+	char *string_data_buf = NULL;
+	char *input_buf = NULL;
+	size_t input_buf_size = 0;
+	
+	// Kullanıcıdan 'MAX_STRING_SIZE - str->size' kadar giriş alınabilir.  
+	// Bunun için önce 'buf' için hafızadan alan alıp daha sonra bu alana kullanıcıdan
+	// bilgi alacağız. Tabi her şey yolunda giderse :)
+	if ((input_buf = (char *)calloc((MAX_STRING_SIZE - str->size),sizeof(char))) == NULL) {
+		return 0;
+	}
+	if (fgets(input_buf, (MAX_STRING_SIZE - str->size), stdin) == NULL) {
+		free(input_buf);
+		input_buf = NULL;
         	return 0;
     	}
 
-	char *new_line = strchr(buf, '\n');
+	//Buraya kadar 'buf için hafızadan alan aldık ve kullanıcıdan buraya giriş yapmasını istedik.
+	// fgets giriş sırasında \n karakterini de değişkene aktarıyor, biz de bunu istemiyoruz.
+	// O yüzden \n karakterini bulup onu NULL ile değiştirelim. 
+	char *new_line = strchr(input_buf, '\n');
 	if (new_line != NULL) {
         	*new_line = '\0';
 	}
 
-	size_t buf_size = strlen(buf);
+	input_buf_size = strlen(input_buf);
 	
+	// Eğer 'data' için ilk değer verilecekse bunu burada yapıyoruz. 
     	if (str->data == NULL) {
-        	str->data = strdup(buf);
-        	if (str->data == NULL) {
+        	if ((str->data = strdup(input_buf)) == NULL) {
+			free(input_buf);
+			input_buf = NULL;
 			return 0;
+		} else {
+			free(input_buf);
+			str->size = input_buf_size + 1;
+			return input_buf_size;
 		}
-        str->size = buf_size + 1;
 	} else {
-        	char *data_buf = realloc(str->data, str->size + buf_size);
-        	if (data_buf == NULL) {
+		/*	???
+		if ((string_data_buf = (char *)calloc((input_buf_size + str->size + 1), sizeof(char))) == NULL) {
+			free(input_buf);
+			input_buf = NULL;
+			return 0;
+		} else {
+			strcpy(string_data_buf, str->data);
+			strcat(string_data_buf, " ");
+			strcat(string_data_buf, input_buf);
+			free(str->data);
+			str->data = NULL;
+			
+			if()
+			
+		}
+		*/
+		
+		
+        	string_data_buf = realloc(str->data, str->size + input_buf_size);
+        	if (string_data_buf == NULL) {
             		return 0;
         	}
-		if (data_buf != str->data) {
+		if (string_data_buf != str->data) {
 			free(str->data);
-			str->data = data_buf;
+			str->data = string_data_buf;
 		}
         
-	        strcat(str->data, buf);
-	        str->size += buf_size;
+	        strcat(str->data, input_buf);
+	        str->size += input_buf_size;
     	}
 
-    return buf_size;
+    return input_buf_size;
 }
 
 void free_string(string *str)
