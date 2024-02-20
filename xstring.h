41f90ee3a930b8be399f1d123e268d68d6f03a76
Emrah Akgül <45803976+emrhAkgl@users.noreply.github.com>
2024-02-20 14:07:56 +0300

    Update xstring.h

--- xstring.h
+++ xstring.h
@@ -9,16 +9,25 @@ typedef struct string{
 	char 		*data;
 	size_t 		size;
 
-	size_t 		(*add)(struct string *str, const char *_data);
-	int		(*add_from_terminal)(struct string *str);
-	void		(*print)(const struct string *str);
-	void		(*free)(struct string *str);
+	size_t 		(*add)			(struct string *str, const char *_data);
+	int		(*add_from_terminal)	(struct string *str);
+	void		(*print)			(const struct string *str);
+	void		(*free)			(struct string *str);
+	int 		(*pop_back)			(struct string *str);
+	size_t		(*get_size)			(const struct string *str);
+	const char*	(*get_data)			(const struct string *str);
+	void		(*clear)			(struct string *str);
 } string;
 
-string *init_string();
-size_t add_string(string *str, const char *_data);
-int add_string_from_terminal(string *str);
-void print_string(const string *str);
-void free_string(string *str);
+/* FUNCTIONS */
+string		*init_string(void);
+size_t		add_string(string *str, const char *_data);
+int		add_string_from_terminal(string *str);
+void		print_string(const string *str);
+void		free_string(string *str);
+int		pop_back_string(string *str);
+size_t		get_string_size(const string *str);
+const char 	*get_string_data(const string *str);
+void		clear_string(string *str);
 
 #endif
