2f6617685e10cc37e3e2a723d19278a934dd1e70
Emrah Akgül <45803976+akgulemrah@users.noreply.github.com>
2024-05-07 23:12:19 +0300

    Update strutil.h

--- strutil.h
+++ strutil.h
@@ -9,14 +9,18 @@ extern "C" {
 #include <string.h>  /* strlen, strcpy ... */
 #include <stdlib.h>  /* malloc, calloc, realloc ... */
 
+
+#define MAX_STRING_SIZE 4096
+
 typedef struct Str {
 	char	*data;
 	unsigned int is_dynamic : 1;
 } str;
 
-/* FUNCTIONS */
+
+/* FUNCTIONS -> */
 str  	*str_init(void);
-size_t  str_add(str *self, const char *_data);
+int  str_add(str *self, const char *_data);
 size_t  str_input(str *self);
 void    str_print(const str *self);
 void    str_free(str *self);
@@ -27,19 +31,21 @@ const char *str_get_data(const str *self);
 int str_rem_word(str *self, const char *needle);
 
 static inline char* get_dyn_input(size_t max_str_size);
-
-
-/* Henüz yapmadıklarım */
-int swap_word(str *self, char *word1, const char *word2);
-/************************************************ XSTRING.H *************************************************************/
-/************************************************************************************************************************/
-
-#define MAX_STRING_SIZE 4096
+int str_swap_word(str *self, const char *word1, const char *word2);
+/* <- FUNCTIONS */
 
 /*
- * To apply the corresponding functions to function pointers in the str structure.
+ * str_init() - Initializes a new Str structure.
+ *
+ * This function initializes a new Str structure and allocates memory for it.
+ * The caller is responsible for freeing the returned structure using str_free().
+ * 
+ * Returns:
+ *     A pointer to the newly initialized Str structure, or NULL if memory allocation fails.
  */
-str *str_init() {
+
+str *str_init()
+{
 	str *tmp = (str *)calloc(1, sizeof(str));
 	if (!tmp)
 		return NULL;
@@ -48,26 +54,34 @@ str *str_init() {
 	return tmp;
 }
 
+
 /*
- * Adds @_data to the data member of @self.
- * @self: struct to which we will add @_data to the member named data.
- * @_data: A pointer to the text to be added to @self->data.
- * Note: If 'self->data' is not empty, it appends '_data' with a space at the end.
+ * str_add() - Adds a string to the data member of a Str structure.
+ * @self: Pointer to the Str structure.
+ * @_data: Pointer to the string to be added.
+ *
+ * This function adds the string @_data to the data member of the Str structure @self.
+ * If @self->data is not empty, @_data is appended with a space at the end.
+ * 
+ * Returns:
+ *     0 on successful completion
+ *    -1 if @self or @_data is NULL, or if memory allocation fails
  */
-size_t str_add(str *self, const char *_data) {
+int str_add(str *self, const char *_data)
+{
 	if (self == NULL || _data == NULL)
-		return 0;
+		return -1;
 
 	size_t self_data_size = self->data ? strlen(self->data) : 0;
 	size_t new_size = self_data_size + strlen(_data) + 1; // +2 for null
 
 	if (new_size >= MAX_STRING_SIZE)
-		return 0;
+		return -1;
 
 	char *new_data = realloc(self->data, new_size);
 
 	if (new_data == NULL)
-		return 0;
+		return -1;
 
 	if (self->data == NULL) {
 		strcpy(new_data, _data); // Copy data if it's the first addition
@@ -77,16 +91,23 @@ size_t str_add(str *self, const char *_data) {
 
 	self->data = new_data;
 
-	return strlen(self->data);
+	return 0;
 }
 
 
 /*
- * Adds a char array to the data member of @self from the terminal.
- * If @self->data is not empty, the value received with a space is appended to @self->data.
- * @self: struct containing the destination of the str received from the terminal. (@self->data)
+ * str_input() - Adds a string from the terminal to the data member of a Str structure.
+ * @self: Pointer to the Str structure.
+ *
+ * This function adds a string received from the terminal to the data member of the Str structure @self.
+ * If @self->data is not empty, the received value is appended with a space.
+ * 
+ * Returns:
+ *     The length of the string added to @self->data, or 0 if an error occurs
  */
-size_t str_input(str *self) {
+
+size_t str_input(str *self)
+{
 	if (self == NULL) {
 		self = str_init();
 		if (!self) {
@@ -120,9 +141,20 @@ size_t str_input(str *self) {
 
 
 /*
- * If @self->data is not empty, delete the word at the end.
+ * str_pop_back() - Removes the last character from the string in a Str structure.
+ * @self: Pointer to the Str structure.
+ * @sep: Separator character to be removed.
+ *
+ * This function removes the last occurrence of the separator character @sep 
+ * from the string in the Str structure @self->data. If @self->data is empty or 
+ * does not contain @sep, the function fails with -1.
+ * 
+ * Returns:
+ *     0 on successful completion
+ *    -1 if @self->data is NULL or empty, or if @sep is not found
  */
-int str_pop_back(str *self, char sep) {
+int str_pop_back(str *self, char sep)
+{
 	if (!self->data || strlen(self->data) == 0)
 		return -1;
 
@@ -143,7 +175,8 @@ int str_pop_back(str *self, char sep) {
 /*
  * If @self->data is not empty, it prints the value in it to the terminal.
  */
-void str_print(const str *self) {
+void str_print(const str *self)
+{
 	if (self->data) {
 		printf("%s", self->data);
 		fflush(stdout);
@@ -154,7 +187,8 @@ void str_print(const str *self) {
  * If @self->data is not empty, it returns the number of characters in it.
  * @self: The struct that contains our return value.
  */
-size_t str_get_size(const str *self) {
+size_t str_get_size(const str *self)
+{
     	return (self->data ? strlen(self->data) : 0);
 }
 
@@ -162,14 +196,16 @@ size_t str_get_size(const str *self) {
  * If the 'data' member of the @self parameter is not empty,
  * it returns the 'data' member as 'const char *'.
  */
-const char *str_get_data(const str *self) {
+const char *str_get_data(const str *self)
+{
     	return (const char *)self->data;
 }
 
 /*
  * It only releases @self->data. It does not delete @self;
  */
-void str_clear(str *self) {
+void str_clear(str *self)
+{
 	if (self->data) {
 		free(self->data);
 		self->data = NULL;
@@ -180,7 +216,8 @@ void str_clear(str *self) {
 /*
  * It releases @str.
  */
-void str_free(str *self) {
+void str_free(str *self)
+{
 	if (self) { // Check NULL
 		if (self->data) {
 			free(self->data);
@@ -193,7 +230,22 @@ void str_free(str *self) {
 	}
 }
 
-static inline char* get_dyn_input(size_t max_str_size) {
+
+/*
+ * get_dyn_input() - Dynamically reads input from the terminal.
+ * @max_str_size: Maximum size of the input string to be read.
+ *
+ * This function dynamically reads input from the terminal, allocating memory as needed.
+ * It continues reading characters until EOF or newline ('\n') is encountered, dynamically 
+ * resizing the memory buffer as necessary. The caller is responsible for freeing the 
+ * returned buffer.
+ * 
+ * Returns:
+ *     A dynamically allocated buffer containing the input string read from the terminal,
+ *     or NULL if memory allocation fails or EOF is encountered before any characters are read.
+ */
+static inline char* get_dyn_input(size_t max_str_size)
+{
 	const int CHUNK_SIZE = 10;
 	char* buffer = (char *)calloc(CHUNK_SIZE,  sizeof(char));
 
@@ -239,34 +291,41 @@ static inline char* get_dyn_input(size_t max_str_size) {
 	return result;
 }
 
-
+/*
+ * str_rem_word() - Removes a specific word from the string.
+ * @self: Pointer to the Str structure.
+ * @needle: The word to be removed from @self->data.
+ *
+ * This function removes the specific word @needle from @self->data. 
+ * If @needle cannot be found, the function fails with -1.
+ * 
+ * Returns:
+ *     0 on successful completion
+ *    -1 if @self, @self->data, or @needle is NULL, or if @needle cannot be found
+ *
+ * Note: This function is designed to manage dynamic memory governed by the 
+ * @self->is_dynamic property. The caller is responsible for freeing 
+ * the returned data to continue accessing the modified data.
+ */
 int str_rem_word(str *self, const char *needle)
 {
-	size_t self_data_size = 0;
-	size_t needle_size = 0;
-
         if (!self || !self->data || !needle)
             return -1;
             
-        self_data_size = strlen(self->data);
-        needle_size = strlen(needle);
+        size_t self_data_size = strlen(self->data);
+        size_t needle_size = strlen(needle);
         
         if (needle_size > self_data_size)
             return -1;
             
-        char *L = NULL, *R = NULL;
+        char *L = NULL;
         L = strstr(self->data, needle);
-	
         if(!L)
             return -1;
 
         memmove(L, L + needle_size, self_data_size - (L - self->data) - needle_size + 1);
 	self->data[self_data_size - needle_size] = '\0';
-
-	/* If ‘self’ is dynamic, the operation is successful and 0 is returned.  */
-	if (!self->isdynamic)
-		return 0;
-	
+        
         char *buf = (char*)realloc(self->data, 
                 ((self_data_size - needle_size)) +1);
         
@@ -278,6 +337,59 @@ int str_rem_word(str *self, const char *needle)
 
 	return 0;
 }
+
+
+/*
+ * str_swap_word() - Replaces a specific word with another word.
+ * @self: Pointer to the Str structure.
+ * @word1: The word to be replaced.
+ * @word2: The new word to replace @word1.
+ *
+ * This function replaces the specific word @word1 found in @self->data 
+ * with @word2. If @word1 cannot be found, the function fails with -1.
+ * 
+ * Returns:
+ *     0 on successful completion
+ *    -1 if an error occurred
+ *
+ * Note: This function is designed to manage dynamic memory governed by the 
+ * @self->is_dynamic property. The caller is responsible for freeing 
+ * the returned data to continue accessing the modified data.
+ */
+int str_swap_word(str *self, const char *word1, const char *word2)
+{
+    if (!self || !self->data || !self->is_dynamic || !word1 || !word2)
+        return -1;
+
+    size_t self_data_size = strlen(self->data);
+    size_t word1_size = strlen(word1);
+    size_t word2_size = strlen(word2);
+
+    char *L = strstr(self->data, word1);
+    if (!L)
+        return -1;
+
+    size_t new_size = self_data_size - word1_size + word2_size;
+    char *buf = (char *)malloc(sizeof(char) * (new_size + 1)); // +1 for null terminator
+    if (!buf)
+        return -1;
+
+    // Copy everything before word1
+    strncpy(buf, self->data, L - self->data);
+    buf[L - self->data] = '\0'; // Null terminate the buffer
+
+    // Copy word2
+    strcat(buf, word2);
+
+    // Copy everything after word1
+    strcat(buf, L + word1_size);
+
+    free(self->data);
+    self->data = buf;
+
+    return 0;
+}
+
 
 #ifdef __cplusplus
 }
