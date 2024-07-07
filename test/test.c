9ab33bcf95cc9a9c34f78e8c86d1c9052136af34
Emrah Akgül <45803976+akgulemrah@users.noreply.github.com>
2024-07-07 21:41:44 +0300

    Update test.c

--- test/test.c
+++ test/test.c
@@ -1,152 +1,204 @@
 #include <stdio.h>
 #include <string.h>
+
 #include "strutil.h"
 
-void test_str_init()
+#define STR_PRINTERR() do {											\
+	fprintf(stderr, "\033[31m%s failed, file: %s, line: %d\033[0m\n", __func__, __FILE__, __LINE__);	\
+} while (0)
+
+unsigned int test_count = 1;
+
+str *test_str_init()
 {
 	str *s = str_init();
-	if (s == NULL) {
-		printf("str_init test failed: returned NULL\n");
-		return;
+	if (s == NULL || s->is_dynamic != 1) {
+		STR_PRINTERR();
+		str_clear(s);
+		return NULL;
 	}
-	if (s->data != NULL) {
-		printf("str_init test failed: data member not initialized to NULL\n");
-		return;
-	}
-	if (s->is_dynamic != 1) {
-		printf("str_init test failed: is_dynamic member not set to 1\n");
-		return;
-	}
-	str_free(s);
-	printf("str_init test passed\n");
+	printf("Test %d test_str_init test passed\n", test_count++);
+	fflush(stdout);
+	return s;
 }
 
-void test_str_add()
+void test_str_add(str *s)
 {
-	str *s = str_init();
-	if (s == NULL) {
-		printf("str_add test failed: str_init failed\n");
+	if (s == NULL || s->data != NULL) {
+		STR_PRINTERR();
 		return;
 	}
 	if (str_add(s, "Hello") != 0) {
-		printf("str_add test failed: unable to add string\n");
-		str_free(s);
+		STR_PRINTERR();
+		str_clear(s);
 		return;
 	}
 	if (strcmp(s->data, "Hello") != 0) {
-		printf("str_add test failed: incorrect string added\n");
-		str_free(s);
+		STR_PRINTERR();
+		str_clear(s);
 		return;
 	}
-	str_free(s);
-	printf("str_add test passed\n");
+
+	str_clear(s);
+	printf("Test %d test_str_add test passed\n", test_count++);
+	fflush(stdout);
 }
 
-void test_str_pop_back()
+void test_str_input(str *s)
 {
-	str *s = str_init();
-	if (s == NULL) {
-		printf("str_pop_back test failed: str_init failed\n");
+	if (s == NULL || s->data != NULL) {
+		STR_PRINTERR();
 		return;
 	}
-	str_add(s, "Hello");
-	str_add(s, " ");
-	str_add(s, "World");
+	printf("test_str_input, write \"Hello\" and press enter: ");
+	fflush(stdout);
+
+	int res = str_input(s);
+	if (res != 0) {
+		STR_PRINTERR();
+		str_clear(s);
+		return;
+	}
+
+	if (strcmp(s->data, "Hello") != 0) {
+		STR_PRINTERR();
+		fprintf(stderr, "expected: \"Hello\", entered: %s\n", s->data);
+		str_clear(s);
+		return;
+	}
+	str_clear(s);
+	printf("Test %d test_str_input passed\n", test_count++);
+	fflush(stdout);
+}
+
+void test_str_pop_back(str *s)
+{
+	if (s == NULL || s->data != NULL) {
+		STR_PRINTERR();
+		return;
+	}
+	int res = str_add(s, "Hello");
+	if (res) {
+		STR_PRINTERR();
+		str_clear(s);
+		return;
+	}
+
+	int res2 = str_add(s, " ");
+	if (res2) {
+		STR_PRINTERR();
+		str_clear(s);
+		return;
+	}
+
+	int res3 = str_add(s, "World");
+	if (res3) {
+		STR_PRINTERR();
+		str_clear(s);
+		return;
+	}
+
 	if (str_pop_back(s, ' ') != 0) {
-		printf("str_pop_back test failed: unable to remove separator\n");
-		str_free(s);
-		return;
-	}
-	if (strcmp(s->data, "Hello") != 0) {
-		printf("str_pop_back test failed: incorrect string after removal\n");
-		str_free(s);
-		return;
-	}
-	str_free(s);
-	printf("str_pop_back test passed\n");
-}
-
-void test_str_get_size()
-{
-	str *s = str_init();
-	if (s == NULL) {
-		printf("str_get_size test failed: str_init failed\n");
-		return;
-	}
-	if (str_add(s, "Hello") != 0) {
-		printf("str_get_size test failed: str_add failed\n");
-		str_free(s);
-		return;
-	}
-	if (str_get_size(s) != 5) {
-		printf("str_get_size test failed: incorrect size returned\n");
-		str_free(s);
-		return;
-	}
-	str_free(s);
-	printf("str_get_size test passed\n");
-}
-
-void test_str_rem_word()
-{
-	str *s = str_init();
-	if (s == NULL) {
-		printf("str_rem_word test failed: str_init failed\n");
-		return;
-	}
-	if (str_add(s, "Hello World") != 0) {
-		printf("str_rem_word test failed: str_add failed\n");
-		str_free(s);
-		return;
-	}
-	if (str_rem_word(s, "World") != 0) {
-		printf("str_rem_word test failed: unable to remove word\n");
-		str_free(s);
+		STR_PRINTERR();
+		str_clear(s);
 		return;
 	}
 	if (strcmp(s->data, "Hello ") != 0) {
-		printf("str_rem_word test failed: incorrect string after removal\n");
-		str_free(s);
+		STR_PRINTERR();
+		str_clear(s);
 		return;
 	}
-	str_free(s);
-	printf("str_rem_word test passed\n");
+	str_clear(s);
+	printf("Test %d test_str_input passed\n", test_count++);
+	fflush(stdout);
 }
 
-void test_str_swap_word()
+void test_str_get_size(str *s)
 {
-	str *s = str_init();
-	if (s == NULL) {
-		printf("str_swap_word test failed: str_init failed\n");
+	if (s == NULL || s->data != NULL) {
+		STR_PRINTERR();
+		return;
+	}
+	if (str_add(s, "Hello") != 0) {
+		STR_PRINTERR();
+		str_clear(s);
+		return;
+	}
+	if (str_get_size(s) != 5) {
+		STR_PRINTERR();
+		str_clear(s);
+		return;
+	}
+	str_clear(s);
+	printf("Test %d test_str_input passed\n", test_count++);
+	fflush(stdout);
+}
+
+void test_str_rem_word(str *s)
+{
+	if (s == NULL || s->data != NULL) {
+		STR_PRINTERR();
 		return;
 	}
 	if (str_add(s, "Hello World") != 0) {
-		printf("str_swap_word test failed: str_add failed\n");
-		str_free(s);
+		STR_PRINTERR();
+		str_clear(s);
+		return;
+	}
+	if (str_rem_word(s, "World") != 0) {
+		STR_PRINTERR();
+		return;
+	}
+	if (strcmp(s->data, "Hello ") != 0) {
+		STR_PRINTERR();
+		str_clear(s);
+		return;
+	}
+	str_clear(s);
+	printf("Test %d test_str_input passed\n", test_count++);
+	fflush(stdout);	
+}
+
+void test_str_swap_word(str *s)
+{
+	if (s == NULL || s->data != NULL) {
+		STR_PRINTERR();
+		return;
+	}
+
+	if (str_add(s, "Hello World") != 0) {
+		STR_PRINTERR();
+		str_clear(s);
 		return;
 	}
 	if (str_swap_word(s, "Hello", "Hi") != 0) {
-		printf("str_swap_word test failed: unable to swap word\n");
-		str_free(s);
+		STR_PRINTERR();
+		str_clear(s);
 		return;
 	}
 	if (strcmp(s->data, "Hi World") != 0) {
-		printf("str_swap_word test failed: incorrect string after swap\n");
-		str_free(s);
+		STR_PRINTERR();
+		str_clear(s);
 		return;
 	}
-	str_free(s);
-	printf("str_swap_word test passed\n");
+	str_clear(s);
+	printf("Test %d test_str_input passed\n", test_count++);
+	fflush(stdout);	
 }
 
 int main()
 {
-	test_str_init();
-	test_str_add();
-	test_str_pop_back();
-	test_str_get_size();
-	test_str_rem_word();
-	test_str_swap_word();
+	str *s = test_str_init();
+	if (!s)
+		return EXIT_FAILURE;
+
+	test_str_add(s);
+	test_str_input(s);
+	test_str_pop_back(s);
+	test_str_get_size(s);
+	test_str_rem_word(s);
+	test_str_swap_word(s);
 	
+	str_free(s);
 	return 0;
 }
